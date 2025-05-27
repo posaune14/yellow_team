d5a5ffc28f2f6c6b6805f0ae7fa9737ebde462c2
Joshy14 <Joshua.sambol14@gmail.com>
2025-05-27 18:24:29 -0400

    Create ios project folder

---
+++ PantryLink/PantryLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PantryLink
+//
+//  Created by Joshua Sambol on 5/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
