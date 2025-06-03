af86e699016296f1e5cc0f05f87d0fd2016dcbeb
posaune14 <michaelyoutz3@gmail.com>
2025-06-03 17:02:50 -0400

    first two text boxes and the form and scroll view

--- PantryLink/PantryLink/ContentView.swift
+++ PantryLink/PantryLink/ContentView.swift
@@ -6,16 +6,37 @@
 //
 
 import SwiftUI
+//add colors here
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            
+            //title
+            VStack{
+                Text("Start Volunteering").font(.system(size: 48, weight: .black, design: .serif)).foregroundStyle(.white).multilineTextAlignment(.center)
+                Text("Today").font(.system(size: 48, weight: .light, design: .rounded)).foregroundStyle(.green)
+            }.padding(20)
+            
+            //paragraph
+            VStack{
+                Text("Earn community service hours, respect, and a good feeling").font(.system(size: 40, weight: .bold, design: .serif)).foregroundStyle(.white)
+            }
+            
+            //register form
+            Form{
+                Section(header: Text("Register as a Volunteer").font(.system(size: 36, weight: .thin, design: .rounded)).foregroundStyle(.green).multilineTextAlignment(.center)){
+                    //textfield
+                }
+            }
+            .frame(width: 300, height: 1000)
+            .padding(20)
+          
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.gray)
+
     }
 }
 
