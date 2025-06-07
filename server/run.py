8c591c26fa31f305cd5dbf286982c1dbea5a62db
naishasinghh <naishasingh210@gmail.com>
2025-06-07 10:41:20 -0400

    Move the creation of app into its own module

--- server/run.py
+++ server/run.py
@@ -1,18 +1,15 @@
 from flask import Flask
-
+from app import create_app
 
 
 if __name__ == "__main__": 
     print("Starting the server...")
-    app = Flask("FoodSecurityServer")
+    app = create_app()
     @app.route("/")
     def hello_world():
         return "<p>Hello, World!</p>"
-<<<<<<< Updated upstream
     
-=======
     @app.route("/")
     def test():
         return "<p>Refwljbnfn</p>"
->>>>>>> Stashed changes
     app.run(port=3000, debug=True)
