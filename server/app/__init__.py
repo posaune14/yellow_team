b83c2908e0ff08fbb2f7b3cbfe2c2b50a346067c
naishasinghh <naishasingh210@gmail.com>
2025-06-07 10:47:04 -0400

    Update create_app configuration

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,10 @@
 from flask_cors import CORS
 from flask import Flask
+from routes import init_routes
 
 def create_app():
     #temperary name of project
     app = Flask("Foodbank project")
     CORS(app)
+    init_routes(app)
+    return app
