c242c3a36e35681dd267dbfceb04b320fb65f402
Joshsambol <Joshua.sambol14@gmail.com>
2025-06-10 18:24:25 -0400

    mom

--- ClientReact/src/App.jsx
+++ ClientReact/src/App.jsx
@@ -1,26 +1,23 @@
-import { useState } from 'react'
-import { Center, Container, Text, Title, Fieldset, TextInput, Button } from '@mantine/core'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Center } from '@mantine/core'
+import SigninBox from './components/SigninBox'
+import Landing from './pages/Landing'
+import Dashboard from './components/Dashboard'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <Center>
-        <Container style={{ borderRadius: '40px', backgroundColor: '#fff', height: '35rem', width: '30rem', margin: '1rem' }}>
-          <Title order={1} style={{ padding: '2rem'}}>
-            Food Bank Sign In
-          </Title>
-          <Center>
-            <Fieldset legend="Food bank information" bg="transparent" style={{textAlign:'left', width:"20rem", height: "15rem", margin: '2rem'}}>
-              <TextInput label="Food Bank" placeholder="Enter your username here"   size="xl" />
-              <TextInput label="Password" placeholder="Enter your password here"   size="xl" mt="md" type='password'/>
-            </Fieldset>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <Center> 
+            <Landing />
           </Center>
-
-            <Button margin="xl" varient="light" color="gray" size="md" radius="xl">Sign in</Button>
-        </Container>
-      </Center>
-    </>
+        } />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SigninBox />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ ClientReact/src/components/SigninBox.jsx
@@ -0,0 +1,41 @@
+import { Box, Text, TextInput, Button, Center, Container, Title, Fieldset } from '@mantine/core'
+import { useNavigate } from 'react-router-dom'
+
+function SigninBox() {
+    const navigate = useNavigate()
+
+    const handleSignIn = () => {
+       //backend stuff here - Naisha working on it
+        navigate('/dashboard')
+    }
+
+    return (
+
+        <Center>
+            <Container style={{ borderRadius: '40px', backgroundColor: '#fff', height: '35rem', width: '30rem', margin: '1rem' }}>
+            <Title order={1} style={{ padding: '2rem'}}>
+                Food Bank Sign In
+            </Title>
+            <Center>
+                <Fieldset legend="Food bank information" bg="transparent" style={{textAlign:'left', width:"20rem", height: "15rem", margin: '2rem'}}>
+                <TextInput label="Food Bank" placeholder="Enter your username here"   size="xl" />
+                <TextInput label="Password" placeholder="Enter your password here"   size="xl" mt="md" type='password'/>
+                </Fieldset>
+            </Center>
+
+                <Button 
+                    onClick={handleSignIn}
+                    margin="xl" 
+                    varient="light" 
+                    color="gray" 
+                    size="md" 
+                    radius="xl"
+                >
+                    Sign in
+                </Button>
+            </Container>
+        </Center>
+    )
+}
+
+export default SigninBox
