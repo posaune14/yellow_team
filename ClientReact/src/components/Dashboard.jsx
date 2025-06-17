3d308d4c7e9b9fc16ea492639d3da80ecba714cd
Joshsambol <Joshua.sambol14@gmail.com>
2025-06-17 12:33:18 -0400

    Dashboard settings and containers

--- ClientReact/src/components/Dashboard.jsx
+++ ClientReact/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Container, Title, Text, Button, Center } from '@mantine/core'
+import { Container, Title, Text, Button, Center, Drawer, AppShell, Menu } from '@mantine/core'
 import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
@@ -6,22 +6,48 @@ function Dashboard() {
     // https://dribbble.com/shots/25241984-Task-Management-Dashboard
 
     return (
-        <Center>
-                <Text size="lg" style={{ padding: '2rem', textAlign: 'center' }}>
-                    You have successfully signed in!
-                </Text>
-                <Center>
-                    <Button  
-                        onClick={() => navigate('/')} 
-                        variant="light" 
-                        color="gray" 
-                        size="md" 
-                        radius="xl"
-                    >
-                        Sign Out
-                    </Button>
-                </Center>
-        </Center>
+        <AppShell>
+                        <Button  
+                            onClick={() => navigate('/')} 
+                            variant="light" 
+                            color="gray" 
+                            size="md" 
+                            radius="xl"
+                        >
+                            Sign Out
+                        </Button>
+            <AppShell.Header style={{marginLeft: '24vw'}}>
+            <Container w='90vw' h='7vh' style={{backgroundColor: '#e3e3e3', margin: '2rem', borderRadius: '20px'}}>
+            <Menu>
+                <Menu.Target>
+                    <Button>Settings icon</Button>
+                </Menu.Target>
+                <Menu.Dropdown>
+                    <Menu.Label>Application</Menu.Label>
+                        <Menu.Item>
+                            Itewm
+                        </Menu.Item>
+                    <Menu.Divider />
+                    <Menu.Label>Account</Menu.Label>
+                    <Menu.Item>
+                        Account settings
+                    </Menu.Item>
+                    <Menu.Item onClick={() => navigate('/')} color='red'>
+                        Sign out
+                    </Menu.Item>
+                </Menu.Dropdown>
+            </Menu>
+            </Container>
+            </AppShell.Header>
+            <AppShell.Navbar withBorder={false}>
+                <Container h='90vh' w='20vw' style={{backgroundColor: '#e3e3e3', margin: '2rem', borderRadius: '20px'}}>
+
+                </Container>
+            </AppShell.Navbar>
+            <AppShell.Main>
+
+            </AppShell.Main>
+        </AppShell>
     )
 }
 
