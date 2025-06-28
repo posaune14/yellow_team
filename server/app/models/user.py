f2a7a454501651b67da7868198968807f7840800
naishasinghh <naishasingh210@gmail.com>
2025-06-28 11:56:41 -0400

    Create UserModel class
    Create the module to add the user in the database based off of the information that was imported by the user.

---
+++ server/app/models/user.py
@@ -0,0 +1,9 @@
+
+class UserModel:
+    def __init__(self):
+        print("creating the user in the database")
+
+    def create_user(self, password, username, first_name, last_name, email, phone_number): 
+        print(password)
+        print(username)
+        
