1a55c6caba7b1650d17d5747b17dc124d31ba0c8
posaune14 <michaelyoutz3@gmail.com>
2025-07-01 16:17:45 -0400

    finishing the route and logic

--- server/routes/volunteer_routes.py
+++ server/routes/volunteer_routes.py
@@ -8,9 +8,28 @@ volunteer_routes = Blueprint("volunteer_routes", __name__)
 
 @volunteer_routes.route("/create", methods=["POST"])
 def create_volunteer():
-    data = request.get_json()
-    
-    
-    new_volunteer = volunteer_model(current_app.mongo)
+    try:
+        data = request.get_json()
+        first_name = data["first_name"]
+        last_name = data["last_name"]
+        date_of_birth = data["data_of_birth"]
+        email = data["email"]
+        phone_number = data["phone_number"]
+        zipcode = data["zipcode"]
+        roles = data["roles"]
+        availability = data["availability"]
+        emergency_name = data["emergency_name"]
+        emergency_number = data["emergency_number"]
+        
+        new_volunteer = volunteer_model(current_app.mongo)
 
-    response = new_volunteer.create_volunteer()
+        response = new_volunteer.create_volunteer(first_name, last_name, date_of_birth, email, phone_number, zipcode, roles, availability, emergency_name, emergency_number)
+
+    except Exception as e:
+        return jsonify({"message": "Error creating volunteer", "error": str(e)}), 400
+    
+    return jsonify(
+        {
+            "_id": response 
+        }
+    ), 201
