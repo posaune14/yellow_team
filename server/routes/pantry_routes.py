a09a40e319d9afe5e25b14cbc2636243f1898089
posaune14 <michaelyoutz3@gmail.com>
2025-08-26 16:50:21 -0400

    create route

--- server/routes/pantry_routes.py
+++ server/routes/pantry_routes.py
@@ -3,3 +3,25 @@ from app.models.pantry import pantry_model
 from bson import ObjectId
 
 pantry_routes = Blueprint("pantry_routes", __name__)
+
+@pantry_routes.route("/create", methods=["POST"])
+def create_pantry():
+    try: 
+        data = request.get_json()
+        name = data["name"]
+        address = data["address"]
+        email = data["email"]
+        phone_number = data["phone_number"]
+        password = data["password"]
+
+        new_pantry = pantry_model(current_app.mongo)
+        response = new_pantry.create_pantry(name, address, email, phone_number, password)
+
+    except Exception as e:
+        return jsonify({"message": "Error creating pantry", "error": str(e)}), 400
+    
+    return jsonify(
+        {
+            "_id": response,
+        }
+    ),201
