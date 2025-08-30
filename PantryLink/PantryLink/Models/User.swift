7cd1ec76b67da7cdb53f7aa9acd76fb84b8c8864
naishasinghh <naishasingh210@gmail.com>
2025-08-30 11:32:45 -0400

    Create user model file

---
+++ PantryLink/PantryLink/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Naisha Singh on 7/26/25.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: String
+    let firstname: String
+    let lastname: String
+    let email: String
+    let username: String
+    let password: String
+    let phonenumber: String
+}
