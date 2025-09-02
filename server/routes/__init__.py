4118bcf3963626ec6d917baf3b26595142677756
nbhavsar133 <nprbhvsr@gmail.com>
2025-09-02 17:40:49 -0400

    registered blueprint for auth login

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,6 +1,8 @@
 from .volunteer_routes import volunteer_routes
 from .user_routes import user_routes
+from .auth_routes import auth_routes
 
 def init_routes(app):
     app.register_blueprint(volunteer_routes, url_prefix="/volunteer")
     app.register_blueprint(user_routes, url_prefix="/user")
+    app.register_blueprint(auth_routes, url_prefix="/auth")
