6bc9a4778b5c9708e3736190a41e4ad59428081a
nbhavsar133 <nprbhvsr@gmail.com>
2025-09-02 18:04:13 -0400

    edited blueprint

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -3,4 +3,4 @@ from .pantryauth_route import pantryauth_route
 
 def init_routes(app):
     app.register_blueprint(volunteer_routes, url_prefix="/volunteer")
-    app.register_blueprint(pantryauth_route, url_prefix="/pantry login")
+    app.register_blueprint(pantryauth_route, url_prefix="/pantry_login")
