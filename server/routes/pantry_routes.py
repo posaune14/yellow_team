d447f0002abbc39e245dbf3084271191d883009d
nbhavsar133 <nprbhvsr@gmail.com>
2025-09-03 18:41:38 -0400

    hash password before entering user in database

--- server/routes/pantry_routes.py
+++ server/routes/pantry_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, current_app, request
 #These lines should "register the blueprint"
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from app.models.pantry import PantryModel
 pantry_routes = Blueprint("pantry_routes", __name__)
 
@@ -29,11 +30,13 @@ def post_pantrys():
         pantry_email = data["pantry_email"]
         phone_number = data["phone_number"]
 
+        bcrypt = Bcrypt(current_app)
+        hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
         #creating an object from UserModel
         new_pantry = PantryModel(current_app.mongo)
         
         #creates the new user
-        response = new_pantry.create_user(pantry_name, pantry_email, phone_number, username, password)
+        response = new_pantry.create_user(pantry_name, pantry_email, phone_number, username, hashed_password)
     
     #Checks to make sure no errors occur and break code
     except Exception as e:
