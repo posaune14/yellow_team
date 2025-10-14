14967f1a9f17c89ec70cf2e72aa8736bd29f8579
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-10-14 11:49:25 -0400

    Create new files for Stream functionality

---
+++ PantryLink/PantryLink/Views/PantryAlertView.swift
@@ -0,0 +1,18 @@
+//
+//  PantryAlertView.swift
+//  PantryLink
+//
+//  Created by Jared Sinai Hernandez Adame on 10/14/25.
+//
+
+import SwiftUI
+
+struct PantryAlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PantryAlertView()
+}
