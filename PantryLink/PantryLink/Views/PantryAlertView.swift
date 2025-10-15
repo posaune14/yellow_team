52d0a3ed645bb39c1573b3e74e49122dfcda022c
nbhavsar133 <nprbhvsr@gmail.com>
2025-10-15 19:02:02 -0400

    changing stream alert message

--- PantryLink/PantryLink/Views/PantryAlertView.swift
+++ PantryLink/PantryLink/Views/PantryAlertView.swift
@@ -12,24 +12,47 @@ struct PantryAlertView: View {
     let message: String
     let date: String
     var body: some View {
-        RoundedRectangle(cornerRadius: 10)
-            .fill(.white)
-            .frame(width:300, height:150)
-            .overlay(
-                VStack {
-                    Text(pantryName)
-                        .bold()
-                        .font(.title2)
-                        .padding();
-                    Text(message)
-                        .font(.body)
-                    Text("Time: \(date)")
-                        .font(.body)
-                }
-            )
+        VStack{
+            //message header with name of pantry
+            Text(pantryName)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            
+            
+            //message of pantry aligned to the left
+            Text(message)
+                .font(.title2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                
+            ZStack{
+                
+                
+                
+            }
+                
+            
+            //        RoundedRectangle(cornerRadius: 10)
+            //            .fill(.white)
+            //            .frame(width:300, height:150)
+            //            .overlay(
+            //                VStack {
+            //                    Text(pantryName)
+            //                        .bold()
+            //                        .font(.title2)
+            //                        .padding();
+            //                    Text(message)
+            //                        .font(.body)
+            //                    Text("Time: \(date)")
+            //                        .font(.body)
+            //                }
+            //            )
+        }
     }
 }
 
-//#Preview {
-//    PantryAlertView()
-//}
+#Preview {
+    PantryAlertView(pantryName: "Pantry 1", message: "hello ", date: "10/14/25")
+}
