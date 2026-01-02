9a1be03c3cfb0b0deeedf42e299f98dd08870c1f
Joshsambol <Joshua.sambol14@gmail.com>
2026-01-02 10:09:54 -0500

    Account control UI

---
+++ PantryLink/PantryLink/Views/AccountView.swift
@@ -0,0 +1,97 @@
+//
+//  AccountView.swift
+//  PantryLink
+//
+//  Created by Joshua Sambol on 1/2/26.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        HStack(){
+            Button {
+                print("back")
+            } label: {
+                Image(systemName: "arrowshape.backward.fill")
+            }
+            .foregroundStyle(.black)
+            .padding()
+
+            Text("Account Information")
+                .font(.title2)
+            Spacer()
+        }
+        VStack(){
+            Text("Username")
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .stroke(Color.flexibleDarkGray, lineWidth: 5)
+                .frame(width: 300, height: 50)
+                .foregroundStyle(.flexibleLightGray)
+            
+            Text("First Name")
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .stroke(Color.flexibleDarkGray, lineWidth: 5)
+                .frame(width: 300, height: 50)
+                .foregroundStyle(.flexibleLightGray)
+            
+            Text("Last Name")
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .stroke(Color.flexibleDarkGray, lineWidth: 5)
+                .frame(width: 300, height: 50)
+                .foregroundStyle(.flexibleLightGray)
+            
+            Text("Email")
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .stroke(Color.flexibleDarkGray, lineWidth: 5)
+                .frame(width: 300, height: 50)
+                .foregroundStyle(.flexibleLightGray)
+            
+            Text("Phone Number")
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .stroke(Color.flexibleDarkGray, lineWidth: 5)
+                .frame(width: 300, height: 50)
+                .foregroundStyle(.flexibleLightGray)
+            
+            
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .stroke(Color.flexibleDarkGray, lineWidth: 5)
+                .frame(width: 300, height: 50)
+                .foregroundStyle(Color.flexibleLightGray)
+                .padding(.top, 50)
+                .overlay(Text("Log Out")
+                    .padding(.top, 50)
+                    .bold(true)
+                )
+            
+            
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .stroke(Color(red: 216/255, green: 82/255, blue: 82/255), lineWidth: 5)
+                .frame(width: 300, height: 50)
+                .foregroundStyle(Color(red: 255/255, green: 103/255, blue: 103/255))
+                .padding(.top, 10)
+                .overlay(Text("Delete Account")
+                    .padding(.top, 10)
+                    .bold(true)
+                )
+                
+            
+            
+                
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    AccountView()
+}
